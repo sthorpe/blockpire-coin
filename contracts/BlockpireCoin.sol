416b8a885ad92862ca70adb7cd67d3bec704303f
Scott Thorpe <sthorpe@gmail.com>
2018-04-19 15:25:38 -0700

    Adding BlockpireCoin Contract

---
+++ contracts/BlockpireCoin.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.20;
+
+import 'zeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
+
+contract BlockpireCoin is StandardToken {
+  string public name = 'BlockpireCoin';
+  string public symbol = 'BPC';
+  uint8 public decimals = 2;
+  uint public INITIAL_SUPPLY = 12000;
+
+  function BlockpireCoin() public {
+    totalSupply_ = INITIAL_SUPPLY;
+    balances[msg.sender] = INITIAL_SUPPLY;
+  }
+}
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+var BlockpireCoin = artifacts.require("BlockpireCoin");
+
+module.exports = function(deployer) {
+  deployer.deploy(BlockpireCoin);
+};
