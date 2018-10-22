aefe172f96efaca8d4cb829aae8f0e1fa9940f8d
Scott Thorpe <sthorpe@gmail.com>
2018-10-22 16:27:10 -0400

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,24 @@
-var BlockpireCoin = artifacts.require("BlockpireCoin");
+const BlockpireCoinCrowdsale = artifacts.require('./BlockpireCoinCrowdsale.sol');
+const BlockpireCoin = artifacts.require('./BlockpireCoin.sol');
 
-module.exports = function(deployer) {
-  deployer.deploy(BlockpireCoin);
+module.exports = function(deployer, network, accounts) {
+  const openingTime = web3.eth.getBlock('latest').timestamp + 2; // two secs in the future
+  const closingTime = openingTime + 86400 * 20; // 20 days
+  const rate = new web3.BigNumber(1000);
+  const wallet = accounts[1];
+
+  return deployer
+    .then(() => {
+        return deployer.deploy(BlockpireCoin);
+    })
+    .then(() => {
+      return deployer.deploy(
+          BlockpireCoinCrowdsale,
+          openingTime,
+          closingTime,
+          rate,
+          wallet,
+          BlockpireCoin.address
+      );
+    });
 };
